cac90e3e3a77b7c45e611cbdfcdec748de36533a
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-03-23 18:52:08 +0800

    refactor modify mongoose config

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-// 載入 mongoose 
-const mongoose = require('mongoose')
 // 載入 handlebars
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
 
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/shortenURL', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongoDB error')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
